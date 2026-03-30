8ab07a53bffe4d723612dad723915a8eb32c1cd3
Pavel <78315934+pavelhess73@users.noreply.github.com>
2026-03-30 11:41:26 +0200

    Prvni verze radaru

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(cors()); // Povolí doplňku posílat data
+app.use(express.json()); // Aby server uměl číst data
+
+// Zobrazení učitelského panelu
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Zde server chytá data od žáků a posílá je vám na obrazovku
+app.post('/api/data', (req, res) => {
+    const dataZaka = req.body;
+    io.emit('aktualizacePanelu', dataZaka);
+    res.sendStatus(200);
+});
+
+const PORT = 3000;
+http.listen(PORT, () => {
+    console.log(`Server běží! Učitelský panel otevřete na: http://localhost:${PORT}`);
+});
